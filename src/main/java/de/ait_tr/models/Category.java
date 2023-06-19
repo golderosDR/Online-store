92a706af04abd0e38dbcc4605e1d979a04fb11cd
Konstantin Glazunov <kglaz@ya.ru>
2023-06-19 20:01:43 +0200

    Kosta product, category, txt

---
+++ src/main/java/de/ait_tr/models/Category.java
@@ -0,0 +1,10 @@
+package de.ait_tr.models;
+
+public enum Category {
+
+        Electronics,
+        Watches,
+        Health,
+        Accessories
+
+}
--- src/main/java/de/ait_tr/models/Product.java
+++ src/main/java/de/ait_tr/models/Product.java
@@ -1,4 +1,68 @@
 package de.ait_tr.models;
 
+import de.ait_tr.models.Category;
+
 public class Product {
+
+    private Category category;
+    private String title;
+    private double price;
+    private double margin;
+    private String subscription;
+    private int quantity;
+
+    public Product(String title, double price, double margin, String subscription, int quantity) {
+        this.title = title;
+        this.price = price;
+        this.margin = margin;
+        this.subscription = subscription;
+        this.quantity = quantity;
+    }
+
+    public void setMargin(double margin) {
+        this.margin = margin;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getMargin() {
+        return margin;
+    }
+
+    public String getSubscription() {
+        return subscription;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    @Override
+    public String toString() {
+        return "product{" +
+                "category=" + category +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", margin=" + margin +
+                ", subscription='" + subscription + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
+
